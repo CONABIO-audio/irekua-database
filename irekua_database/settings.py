9b8e91f5a516e12a8dc6ab5fdb4cf1d2ce4c8cd4
mbsantiago <santiago.mbal@gmail.com>
2019-11-28 22:14:35 -0600

    syntax error in settings

--- irekua_database/settings.py
+++ irekua_database/settings.py
@@ -13,7 +13,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'HOST': os.environ.get('IREKUA_DATABASE_HOST', 'localhost'),
         'PORT': os.environ.get('IREKUA_DATABASE_PORT', '5432'),
-        'NAME': os.environ.get('IREKUA_DATABASE_NAME','irekua_dummy'),
+        'NAME': os.environ.get('IREKUA_DATABASE_NAME', 'irekua_dummy'),
         'USER': os.environ.get('IREKUA_DATABASE_USER', 'irekua'),
         'PASSWORD': os.environ.get('IREKUA_DATABASE_PASSWORD', ''),
     }

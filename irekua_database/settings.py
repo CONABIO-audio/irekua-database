48c0d400a3b09b1a1c5dbc3aed2b8c37061c5585
mbsantiago <santiago.mbal@gmail.com>
2019-11-28 21:12:27 -0600

    fixed settings error

--- irekua_database/settings.py
+++ irekua_database/settings.py
@@ -12,7 +12,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'HOST': os.environ.get('IREKUA_DATABASE_HOST', 'localhost'),
-        'PORT': os.environ.get('IREKUA_DATABASE_PORT', '5432')
+        'PORT': os.environ.get('IREKUA_DATABASE_PORT', '5432'),
         'NAME': os.environ.get('IREKUA_DATABASE_NAME','irekua_dummy'),
         'USER': os.environ.get('IREKUA_DATABASE_USER', 'irekua'),
         'PASSWORD': os.environ.get('IREKUA_DATABASE_PASSWORD', ''),
